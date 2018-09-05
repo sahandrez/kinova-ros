e2d90217c2a25a0a37df2e8d5f5d4db838eb3f34
sahand <sahand.rezaei.sh@gmail.com>
2018-09-05 12:06:35 -0400

    Change the plugin name

--- kinova_plugin/src/joint_effort_plugin.cpp
+++ kinova_plugin/src/joint_effort_plugin.cpp
@@ -15,7 +15,7 @@ namespace gazebo
 {
         class ModelPush : public ModelPlugin
         {
-		private: 
+		private:
 			ros::NodeHandle * n;
 			ros::Publisher joint_state_pub;
 			ros::Subscriber joint_effort_sub;
@@ -30,14 +30,14 @@ namespace gazebo
 			// for (int i = 0; i < msg->data.size(); i++)
 			// {
 			// 	std::cout << msg->data[i] << std::endl;
-			// }	
+			// }
 			// std::cout << "===============================" << std::endl;
 			physics::Joint_V joints = this->model->GetJoints();
 			this->pastCommand.clear();
 
 			this->pastCommand.push_back(0);
 			for (int i = 0; i < msg->data.size(); i++) this->pastCommand.push_back(msg->data[i]);
-			
+
 		}
 
                 public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
@@ -49,7 +49,7 @@ namespace gazebo
                                         // simulation iteration.
                                         this->updateConnection = event::Events::ConnectWorldUpdateBegin(
                                                         boost::bind(&ModelPush::OnUpdate, this, _1));
-					this->n = new ros::NodeHandle("kinova_plugin");
+					this->n = new ros::NodeHandle("joint_effort_plugin");
 					this->joint_state_pub = n->advertise<sensor_msgs::JointState>("j2n6s300/joint_state", 1);
 					this->joint_effort_sub = n->subscribe("j2n6s300/joint_effort", 1, &ModelPush::JointEffortCallback, this);
 					this->pastCommandCounter = 0;
@@ -64,7 +64,7 @@ namespace gazebo
 					sensor_msgs::JointState joint_state;
                                         physics::Joint_V joints = this->model->GetJoints();
 
-					//get time					
+					//get time
 					ros::Time current_time = ros::Time::now();
 					joint_state.header.stamp = current_time;
 
@@ -76,28 +76,28 @@ namespace gazebo
 					{
 						for (int i = 0; i < joints.size() && i < this->pastCommand.size(); i++)
 						{
-						     // Force is additive (multiple calls to SetForce to the same joint in the same time step 
+						     // Force is additive (multiple calls to SetForce to the same joint in the same time step
 						     // will accumulate forces on that Joint)?
 						     joints[i]->SetForce(0, this->pastCommand[i]);
 						}
 						this->pastCommandCounter += 1;
-						if (this->pastCommandCounter >= HOLD_FRAMES) 
-						{ 
+						if (this->pastCommandCounter >= HOLD_FRAMES)
+						{
 							this->applyPastCommand = false;
 						}
 					}
 
                                         for (int i = 1; i < joints.size(); i++)
                                         {
-						
+
 						joint_state.name[i-1] = joints[i]->GetName();
 						joint_state.position[i-1] = joints[i]->GetAngle(0).Radian();
 						joint_state.velocity[i-1] = joints[i]->GetVelocity(0);
 
-                                        }														
-					
+                                        }
+
 					this->joint_state_pub.publish(joint_state);
-                                        
+
                                 }
 
                                 // Pointer to the model
@@ -110,4 +110,3 @@ namespace gazebo
         // Register this plugin with the simulator
         GZ_REGISTER_MODEL_PLUGIN(ModelPush)
 }
-
